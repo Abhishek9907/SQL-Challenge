6d6565d498d69080c669f74784d2076a8ea5c4d9
unknown <abhishekmishra7766@gmail.com>
2024-01-05 14:43:01 +0530

    SQL Question and Answer for the Average Sells

--- Easy/Average selling price.sql
+++ Easy/Average selling price.sql
@@ -64,6 +64,39 @@
 -- Average selling price for product 1 = ((100 * 5) + (15 * 20)) / 115 = 6.96
 -- Average selling price for product 2 = ((200 * 15) + (30 * 30)) / 230 = 16.96
 
+
+CREATE TABLE Prices (
+    product_id  INT,
+    start_date  DATE,
+    end_date    DATE,
+    price       INT,
+    PRIMARY KEY (product_id, start_date, end_date)
+);
+
+
+INSERT INTO Prices (product_id, start_date, end_date, price) VALUES
+(1, '2019-02-17', '2019-02-28', 5),
+(1, '2019-03-01', '2019-03-22', 20),
+(2, '2019-02-01', '2019-02-20', 15),
+(2, '2019-02-21', '2019-03-31', 30);
+
+
+
+
+CREATE TABLE UnitsSold (
+    product_id      INT,
+    purchase_date   DATE,
+    units           INT
+);
+
+
+INSERT INTO UnitsSold (product_id, purchase_date, units) VALUES
+(1, '2019-02-25', 100),
+(1, '2019-03-01', 15),
+(2, '2019-02-10', 200),
+(2, '2019-03-22', 30);
+
+
 -- Solution
 SELECT
     us.product_id,
